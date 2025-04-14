6480c09b88d81423f39e28e1e447087f2330148f
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-04-14 16:02:46 +0200

    Created fonction for show more possibilities dependies of user responses

---
+++ JS/projet-futur.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const radios = document.querySelectorAll('input[name="projet-futur"]');
+    const container = document.getElementById("projet-maison");
+  
+    radios.forEach((radio) => {
+      radio.addEventListener("change", () => {
+        const value = radio.value;
+        container.innerHTML = "";
+  
+        if (value === "oui") {
+          container.innerHTML = `
+            <div>
+            <label class="block text-lg text-center mx-auto w-full leading-relaxed mt-4 mb-1.5">Sur la même maison ?</label>
+            <label class="inline-flex items-center space-x-2 text-xl">
+                <input type="radio" name="meme-maison" value="oui" class="radio-button">
+                <span>Oui</span>
+            </label>
+            <label class="inline-flex items-center space-x-2 text-xl">
+                <input type="radio" name="meme-maison" value="non" class="radio-button">
+                <span>Non</span>
+            </label>
+        </div>
+          `;
+        } else if (value === "non") {
+          container.innerHTML = `
+            <div>
+              <label for="raisons" class="block text-lg mb-1 text-center">Pour quelle raison ?</label>
+              <textarea name="raisons" id="raisons" rows="5" cols="45" class="rounded-lg border-solid border-[#bf198c] border-2 shadow-[#C73F67] shadow-md"></textarea>
+            </div>
+          `;
+        }
+      });
+    });
+  });
