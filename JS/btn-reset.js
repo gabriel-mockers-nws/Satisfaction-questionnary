0979b1cf424c7137fe562ee9bf2bd8a5ed6145a6
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-04-17 13:44:16 +0200

    Add function for reset questionry form

---
+++ JS/btn-reset.js
@@ -0,0 +1,11 @@
+const submitBtn = document.getElementById("btn-sub");
+
+submitBtn.addEventListener("click", function () {
+  const forms = document.querySelectorAll("form");
+
+  forms.forEach((frm) => {
+    frm.reset();
+  });
+
+  alert("Merci pour votre retour !");
+});
