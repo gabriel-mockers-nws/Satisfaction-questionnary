9d8778321178f5d6d2c1dd2a4bab2d8f05270329
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-05-18 12:09:50 +0200

    rectify indentation, correction elements

--- elements/header.php
+++ elements/header.php
@@ -1,25 +1,23 @@
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/src/output.css">
-    <link rel="stylesheet" href="/src/custom.css">
-    <link rel="shortcut icon" href="/img/logo_odcvl_150.png" type="icon-odcvl">
-    <a href="../index.php">
-        <img src="../img/logo_odcvl_150.png" alt="logo-odcvl" class="absolute top-0 left-0 m-4 w-[20vw] h-auto sm:w-[15vw] md:w-[10vw] lg:w-[9vw]">
-    </a>
-    <img src="../img/logos-MTS-2.png" alt="logo-maison-tous-séjours" class="absolute top-0 right-0 m-4 w-[20vw] h-auto sm:w-[15vw] md:w-[10vw] lg:w-[9vw]">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="../src/output.css" />
+    <link rel="stylesheet" href="../src/custom.css" />
+    <link rel="shortcut icon" href="/img/logo_odcvl_150.png" type="icon-odcvl" />
     <title>
         <?php if (isset($title)) : ?>
-            <?= $title ?>
+            <?= htmlspecialchars($title) ?>
         <?php else : ?>
             odcvl-satisfaction
-        <?php endif ?>
+        <?php endif; ?>
     </title>
 </head>
-<header>
-
-</header>
 <body>
+    <header class="relative">
+        <a href="../index.php">
+            <img src="../img/logo_odcvl_150.png" alt="logo-odcvl" class="absolute top-0 left-0 m-4 w-[20vw] h-auto sm:w-[15vw] md:w-[10vw] lg:w-[9vw]" />
+        </a>
+        <img src="../img/logos-MTS-2.png" alt="logo-maison-tous-séjours" class="absolute top-0 right-0 m-4 w-[20vw] h-auto sm:w-[15vw] md:w-[10vw] lg:w-[9vw]" />
+    </header>
